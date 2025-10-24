b7d603cae14cb64eda100e007c21d5ac1044eb18
Akihito Kirisaki <kirisaki@klara.works>
2025-10-25 07:33:28 +0900

    feat: Add labels to opecodes

--- include/v4/opcodes.hpp
+++ include/v4/opcodes.hpp
@@ -4,7 +4,7 @@
 /** Tier-0 opcode set (single byte). Immediates are little-endian. */
 enum class Op : std::uint8_t
 {
-#define OP(name, val) name = val,
+#define OP(name, val, _) name = val,
 #include <v4/opcodes.def>
 #undef OP
 };
